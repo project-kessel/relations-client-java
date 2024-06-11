73f74018f2b9728531c2d5d0c2e1a4a84eed6e7b
Mark McLaughlin <mmclaugh@redhat.com>
2024-06-11 13:27:58 +0100

    Remove disposes method that might break a client in another scope.

--- src/main/java/org/project_kessel/relations/client/CDIManagedClients.java
+++ src/main/java/org/project_kessel/relations/client/CDIManagedClients.java
@@ -1,12 +1,10 @@
 package org.project_kessel.relations.client;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
 
 /**
  * A managed bean for providing relations api clients for injection in apps.
- * Performs some lifecycle management.
  * It has the current limitation that only one underlying grpc connection can be configured.
  * However, it is still possible to create more via RelationsGrpcClientsManager directly.
  * This class does nothing unless the client is being managed by a CDI container (e.g. Quarkus)
@@ -23,10 +21,6 @@ public class CDIManagedClients {
         }
 
         return RelationsGrpcClientsManager.forInsecureClients(targetUrl);
-    }
-
-    void shutdownClientsManager(@Disposes RelationsGrpcClientsManager manager) {
-        RelationsGrpcClientsManager.shutdownManager(manager);
     }
 
     @Produces
